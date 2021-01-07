c5f25f5ac5f7a05d9e566db2f56fea0c8e790724
Karthikb777 <karthik.barati.777@gmail.com>
2021-01-07 09:47:51 +0530

    fixed some alignment things

--- src/components/Sp.js
+++ src/components/Sp.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
-import {
-    Link
-  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import img1 from './img/covid19karLightScCropped.png';
 import img2 from './img/yeet.png';
 
@@ -125,7 +123,7 @@ function Sp() {
                 className="kb-projects">
                         <img className="kb-img img1" src={img1} alt="covid19karnataka"></img>
                         <div className="project1">
-                            <p class="kb-header-small-non-center"><a className="kb-link" target="_blank" rel="noreferrer" href="https://karthikb777.github.io/covid19karnataka/">COVID19KARNATAKA</a></p>
+                            <p className="kb-header-small-non-center"><a className="kb-link" target="_blank" rel="noreferrer" href="https://karthikb777.github.io/covid19karnataka/">COVID19KARNATAKA</a></p>
                             <p className="kb-content-left">This project was created because, I didn't have a quick and easy way to get detailed information of covid 19 statistics of my native state of Karnataka, India. This site is Built with Reactjs and gets the data from an open source API from covid19india.org. <br/><br />Made with: HTML5, CSS3, Javascript and ReactJS.<br /><br /> You can check out the site <a className="kb-link-logo" target="_blank" rel="noreferrer" href="https://karthikb777.github.io/covid19karnataka/">here.</a> <br /> The link to the github repository is <a target="_blank" rel="noreferrer" className="kb-link-logo" href="https://github.com/Karthikb777/covid19karnataka/">here.</a> </p>
                         </div>
                         <img className="kb-img img2" src={img2} alt="yeet"></img>
@@ -139,13 +137,12 @@ function Sp() {
                         </div>
                 </motion.div>
                 <motion.p className="kb-gh-link kb-content-center">Check out my github account for more projects. <a target="_blank" rel="noreferrer" className="kb-link-logo" href="https://github.com/Karthikb777/">Here's the link to my github account.</a></motion.p>
-                <div className="kb-skills-text">
                 <motion.div
                      initial={{ opacity: 0, y: 20 }}
                      animate={{ opacity: 1, y: 0 }}
                      transition={{ delay: 2.7 }}
                      className=""><Link className='kb-link kb-nxtpg' to='/portfolio/contact'>Get in touch {'>'} </Link></motion.div>
-            </div></div>
+            </div>
             </motion.div>
         </>
     );
